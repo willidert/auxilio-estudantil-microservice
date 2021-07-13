784591f5e54275d376ce16dbaf9bd5618eaffa2e
Mateus Bentes <mateus1337bentes@gmail.com>
2021-07-13 19:14:52 -0400

    feat(api): add UserForm Schema

---
+++ api/src/models/UserFormQuestions.ts
@@ -0,0 +1,26 @@
+import mongoose, { Model } from 'mongoose';
+import { UserFormQuestionsModel } from '../interface/UserFormQuestionsModel';
+
+const schema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: [true, 'Email must be unique'],
+    },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+export const User: Model<UserFormQuestionsModel> = mongoose.model(
+  'UserFormQuestion',
+  schema
+);
