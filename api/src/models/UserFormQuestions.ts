d633a5dfc4b75dd64997fb0de353f614eea00fd6
Mateus Bentes <mateus1337bentes@gmail.com>
2021-07-17 16:08:51 -0400

    refactor(api): change attributes to match json

--- api/src/models/UserFormQuestions.ts
+++ api/src/models/UserFormQuestions.ts
@@ -3,6 +3,25 @@ import { UserFormQuestionsModel } from '../interface/UserFormQuestionsModel';
 
 const schema = new mongoose.Schema(
   {
+    questionOne: { type: String },
+    questionTwo: { type: String },
+    questionThree: { type: String },
+    questionFour: { type: String },
+    questionFive: { type: String },
+    questionSix: { type: String },
+    questionSeven: { type: String },
+    questionEight: { type: String },
+    questionNine: { type: String },
+    questionTen: { type: String },
+    questionEleven: { type: String },
+    questionTwelve: { type: String },
+    questionThirteen: { type: String },
+    questionFourteen: { type: String },
+    questionFifteen: { type: String },
+    questionSixteen: { type: String },
+    questionSeventeen: { type: String },
+    questionEighteen: { type: String },
+    questionNineteen: { type: String },
     email: {
       type: String,
       required: true,
