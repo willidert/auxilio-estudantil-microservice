8c95ceabc3a5fe82ea30de5a63188cc64ee2a43c
Mateus Bentes <mateus1337bentes@gmail.com>
2021-07-17 16:11:39 -0400

    feat(api): create routes and controller to handle

--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from 'express';
+import UserFormController from '../controller/UserFormController';
 
 export const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hello World',
-  });
+  UserFormController.index(req, res);
 });
+
+router.post('/', (req: Request, res: Response) =>
+  UserFormController.create(req, res)
+);
