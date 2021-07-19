ee3d434c2ab2547ec3c7e546ac3ae091b154aa66
William Teixeira <williderteixeira@gmail.com>
2021-07-18 20:00:05 -0400

    feat: adding messenger(rabbitMQ) to the model service.

--- model/main.py
+++ model/main.py
@@ -1,20 +1,39 @@
-from flask import Flask, jsonify, request
-import pickle
+import json
+import pika, sys, os
 
-model = pickle.load(open('./model.sav', 'rb'))
+from model import make_prediction
+from database import Database
 
-app = Flask(__name__)
 
-@app.route('/predicao/', methods=['POST'])
-def predict():
-	data = request.get_json()
-	x = data['dados']
-	res = model.predict(x)
-	print(res)
-	return jsonify(
-		res=int(res[0])
-	)
+def main():
+    try:
+        credentials = pika.PlainCredentials(username='admin', password='admin')
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
+    except Exception:
+        return 'deu ruim'
+    channel = connection.channel()
 
+    channel.queue_declare(queue='model')
+
+    def callback(ch, method, properties, body):
+        db = Database()
+        print(" [x] Received %r" % body)
+        email = 'teste@gmail.com'
+        data = json.loads(body)
+        res = make_prediction(data["dados"])
+        db.update(email, res)
+
+    channel.basic_consume(queue='model', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ model/model.py
@@ -0,0 +1,10 @@
+import pickle
+
+from singleton import SingletonMeta
+
+class Model(classmeta=SingletonMeta):
+    def __init__(self) -> None:
+        self.__model = pickle.load(open('model.sav', 'rb'))
+
+    def make_prediction(self, data: list) -> int:
+        return self.__model.predict(data)[0]
